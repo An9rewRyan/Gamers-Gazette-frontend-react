f5b4e163fab7c74ae543e7cd55c89be5e18b2a4d
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-16 14:08:08 +0300

    ADD: redirect import

--- src/controllers/Signup.js
+++ src/controllers/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate
  } from 'react-router-dom';
  import Cookies from 'universal-cookie';
+ import { Redirect } from 'react-router';
 
 const cookies = new Cookies();
 
