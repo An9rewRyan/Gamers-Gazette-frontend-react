ffe2643575e7b5596926f624c06be2dc0b49bb6d
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-19 11:17:52 +0300

    TRY: fix import err

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,71 +1,66 @@
-// import React from 'react';
-// import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
+import React from 'react';
+import Cookies from 'universal-cookie';
 
-// class Logout extends React.Component {
+const cookies = new Cookies();
+class Logout extends React.Component {
 
-//     constructor(props) {
-//       super(props);
-//       this.handleLogout = this.handleLogout(this)
-//       this.state = {already_logged_in: null};
-//     }
+    constructor(props) {
+      super(props);
+      this.handleLogout = this.handleLogout(this)
+      this.state = {already_logged_in: null};
+    }
 
-//     static propTypes = {
-//       cookies: instanceOf(Cookies).isRequired
-//     };
+    componentDidMount() {
+      let session_cookie = cookies.get('session_token')
+      console.log(session_cookie)
+      if (session_cookie){
+        fetch(
+          `https://api-gamersgazette.herokuapp.com/auth/me`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'text/plain'
+            },
+              body: JSON.stringify({session_cookie:session_cookie})
+            })
+              .then((res) =>{
+                console.log(res.status)
+                if (res.status != 400 && res.status != 401){
+                  this.setState({already_logged_in: true})
+                }
+              })
+              .catch((err)=>console.log(err))
+        }
+    }
 
-//     componentDidMount() {
-//       const { cookies } = this.props;
-//       let session_cookie = cookies.get('session_token')
-//       console.log(session_cookie)
-//       if (session_cookie){
-//         fetch(
-//           `https://api-gamersgazette.herokuapp.com/auth/me`, {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'text/plain'
-//             },
-//               body: JSON.stringify({session_cookie:session_cookie})
-//             })
-//               .then((res) =>{
-//                 console.log(res.status)
-//                 if (res.status != 400 && res.status != 401){
-//                   this.setState({already_logged_in: true})
-//                 }
-//               })
-//               .catch((err)=>console.log(err))
-//         }
-//     }
+    handleLogout() {
+      let session_cookie = cookies.get('session_token')
+      cookies.set("session_cookie", "", {expires: "Thu, 01 Jan 1970 00:00:00 UTC"})
+      fetch(
+        `https://api-gamersgazette.herokuapp.com/auth/logout`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'text/plain'
+          },
+            body: JSON.stringify({session_cookie:session_cookie})
+          })
+          .then((res) =>{
+            console.log(res.status)
+            this.setState({already_logged_in: null})
+          })
+          .catch((err)=>console.log(err))
+    }
 
-//     handleLogout() {
-//       const { cookies } = this.props;
-//       let session_cookie = cookies.get('session_token')
-//       cookies.set("session_cookie", "", {expires: "Thu, 01 Jan 1970 00:00:00 UTC"})
-//       fetch(
-//         `https://api-gamersgazette.herokuapp.com/auth/logout`, {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'text/plain'
-//           },
-//             body: JSON.stringify({session_cookie:session_cookie})
-//           })
-//           .then((res) =>{
-//             console.log(res.status)
-//             this.setState({already_logged_in: null})
-//           })
-//           .catch((err)=>console.log(err))
-//     }
+    render() {
+      let {already_logged_in} = this.state;
+        return (
+          <div>
+          {already_logged_in 
+            ? <button onClick={this.handleLogout}>Log out</button>
+            : <div>Logged out!</div>
+          }
+          </div>
+        );
+      }
+    }
 
-//     render() {
-//       let {already_logged_in} = this.state;
-//         return (
-//           <div>
-//           {already_logged_in 
-//             ? <button onClick={this.handleLogout}>Log out</button>
-//             : <div>Logged out!</div>
-//           }
-//           </div>
-//         );
-//       }
-//     }
-
-// export default Logout;
+export default Logout;
